f3cf2b0dc97cc713210e393adb37d4c4385eccd1
Klaus Ahrenberg <klaus.ahrenberg@web.de>
2024-05-06 07:19:10 +0200

    0.01

--- lib/lych.dart
+++ lib/lych.dart
@@ -2,7 +2,6 @@ library lych;
 
 export "src/json.dart";
 export "src/log.dart";
-export "src/material.dart";
 export "src/observables.dart";
 export "src/reflections.dart";
 export "src/repository.dart";
